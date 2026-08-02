4721fcf483ef2240316d2ea171911742b693de72
Scramix <luckybluedog7@gmail.com>
2026-08-02 09:01:35 -0600

    Added error handling to dosbox.js (READ DESC)
    So, I did added/fixed these things:
    
    Somewhat messy lines of code
    Error handling
    Fixed the way JS-DOS runs AUTOEXEC.BAT

--- win/dosbox.js
+++ win/dosbox.js
@@ -8,14 +8,28 @@ console.log("Starting download...");
 const response = await fetch("win.zip");
 const fetchauto = await fetch("AUTOEXEC.BAT");
 
+if (!response.ok) {
+    throw new Error("Could not download win.zip");
+}
+
+if (!fetchauto.ok) {
+    throw new Error("Could not download AUTOEXEC.BAT");
+}
+
 const bytes = await response.arrayBuffer();
 const autoexec = await fetchauto.text();
 
 const files = w.fflate.unzipSync(new Uint8Array(bytes));
 const image = files["CDRIVE.IMG"];
 
-dos.fs.writeFile("CDRIVE.IMG", image);
-dos.fs.writeFile("AUTOEXEC.BAT", autoexec);
 
-dos.run("AUTOEXEC.BAT");
+if (!image) {
+    throw new Error("CDRIVE.IMG not found in win.zip");
+}
 
+console.log ("CDRIVE.IMG extracted. Loading Windows.")
+
+await dos.fs.writeFile("CDRIVE.IMG", image);
+await dos.fs.writeFile("AUTOEXEC.BAT", autoexec);
+
+await dos.shell("AUTOEXEC.BAT");
