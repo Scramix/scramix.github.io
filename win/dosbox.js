22375fab32eaf96a64211c77caa75a902800af60
Scramix <luckybluedog7@gmail.com>
2026-08-02 14:48:41 -0600

    Fixed some stuff in dosbox.js

--- win/dosbox.js
+++ win/dosbox.js
@@ -1,9 +1,5 @@
 const w = window;
 
-const dos = await w.Dos(document.getElementById("dos")).ready;
-
-console.log(dos);
-
 console.log("Booting Windows 3.1...");
 console.log("Starting download...");
 
@@ -24,14 +20,25 @@ const autoexec = await fetchauto.text();
 const files = w.fflate.unzipSync(new Uint8Array(bytes));
 const image = files["CDRIVE.IMG"];
 
-
 if (!image) {
     throw new Error("CDRIVE.IMG not found in win.zip");
 }
 
-console.log ("CDRIVE.IMG extracted. Loading Windows.")
+console.log("CDRIVE.IMG extracted. Loading Windows.");
 
-await dos.fs.writeFile("CDRIVE.IMG", image);
-await dos.fs.writeFile("AUTOEXEC.BAT", autoexec);
+const dos = w.Dos(document.getElementById("dos"), {
+    initFs: [
+        {
+            path: "CDRIVE.IMG",
+            contents: image
+        },
+        {
+            path: "AUTOEXEC.BAT",
+            contents: new TextEncoder().encode(autoexec)
+        }
+    ]
+});
+
+await dos.ready;
 
 await dos.shell("AUTOEXEC.BAT");
